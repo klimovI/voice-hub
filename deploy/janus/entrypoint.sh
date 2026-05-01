687df1cf0ce4d8f2dea4eb6db684f7ab29a35f41
klimovI <58362300+klimovI@users.noreply.github.com>
2026-05-01 11:47:23 +0800

    Janus entrypoint: handle read-only config mount
    Non-root user can't sed -i over a bind-mounted read-only file in dev compose.
    Write transformed config to a writable copy and exec janus from there if
    the in-place rewrite fails.

--- deploy/janus/entrypoint.sh
+++ deploy/janus/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 set -e
 
-CFG=/etc/janus/janus.jcfg
+SRC=/etc/janus/janus.jcfg
+DST=/tmp/janus.jcfg
 
 if [ -n "$PUBLIC_IP" ]; then
-  sed -i "s/__PUBLIC_IP__/${PUBLIC_IP}/" "$CFG"
+  sed "s/__PUBLIC_IP__/${PUBLIC_IP}/" "$SRC" > "$DST"
 else
-  sed -i '/__PUBLIC_IP__/d' "$CFG"
+  sed '/__PUBLIC_IP__/d' "$SRC" > "$DST"
 fi
+
+cp "$DST" "$SRC" 2>/dev/null || {
+  mkdir -p /tmp/janus-cfg
+  cp /etc/janus/*.jcfg /tmp/janus-cfg/
+  cp "$DST" /tmp/janus-cfg/janus.jcfg
+  exec /usr/bin/janus -F /tmp/janus-cfg
+}
 
 exec /usr/bin/janus -F /etc/janus
